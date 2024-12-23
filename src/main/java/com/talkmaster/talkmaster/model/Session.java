e6be1bc1a92d0cafd3dd2923a13d4a4e3a2d4915
Risina Surandi <risinasurandi@gmail.com>
2024-12-23 14:04:21 +0530

    Started working on sessions.

---
+++ src/main/java/com/talkmaster/talkmaster/model/Session.java
@@ -0,0 +1,14 @@
+package com.talkmaster.talkmaster.model;
+
+import java.sql.Date;
+
+public class Session {
+
+    private String instructorId;
+    private String studentId;
+    private Date date;
+    private String start;
+    private String end;
+
+    
+}
